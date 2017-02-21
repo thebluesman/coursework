083afcff13e9e0e4984f7f9d86ed052904b45b72
thebluesman <262.shyam@gmail.com>
2017-02-21 16:21:30 +0400

    Updated revision file

--- BlackJack_revision.py
+++ BlackJack_revision.py
@@ -11,3 +11,35 @@ restart_phrase = "Press 'd' to deal the cards again, or press 'q' to quit"
 suits = ("H", "A", "S", "D")
 ranking = ("A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K")
 card_val = {"A": 1, "2": 2, "3": 3, "4": 4, "5": 5, "6": 6, "7": 7, "8": 8, "9": 9, "10": 10, "J": 10, "Q": 10, "K": 10}
+
+class Card:
+
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+
+    def __str__(self):
+        return self.suit + self.rank
+
+    def grab_suit(self):
+        return self.suit
+
+    def grab_rank(self):
+        return self.rank
+
+    def draw(self):
+        print (self.suit + self.rank)
+
+class Hand:
+
+    def __init__(self):
+        self.cards = []
+        self.value = 0
+        self.ace = False
+
+    def __str__(self):
+        hand_comp = " "
+
+        for card in self.cards:
+            card_name = card.__str__()
+            hand_comp += " " + card_name
